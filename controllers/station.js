b5e38c28070fd6fd41b7e5589114e9045904d9dd
MyeongSooChoi <mongus.c@gmail.com>
2020-04-23 18:35:07 +0900

    - 충전소 관련 정보를 컨트롤 하는 station 모듈 추가  ㄴ 공공 데이터를 파싱하고 db에 저장하는 기능 추가

---
+++ controllers/station.js
@@ -0,0 +1,55 @@
+const parseString = require('xml2js').parseString;
+const lodash = require('lodash');
+const got = require('got');
+
+const { station } = require('../models');
+
+// 0번 아이디 데이터를 조회해 데이터가 없으면 공공데이터 입력
+const insert = (async () => {
+    try {
+        const exData = await station.findOne({ where : { id : '0' } });
+        if(exData) {
+            // 현재 데이터가 들어가 있음
+            console.log('already insert data');
+            return;
+        }
+        const res = await got.get(process.env.API_LINK);
+        let dataArry = [];
+        await parseString(res.body, (err, result) => {
+            if(err) { throw err; }
+            dataArry = result.response.body[0].items[0].item;
+            dataArry = lodash.uniqBy(dataArry, (item) => {
+                return item.statNm[0];
+            });
+        });
+        let index = 0;
+        // await dataArry.forEach((item) => {
+        //     //console.log(index + ' ');
+        //     // console.log(index + ' ' + /*item.statNm[0] + ' , ' + */item.useTime/*[0]*/);
+        //     // ++index;
+        //     if(item.useTime === undefined) {
+        //         console.log(item.statNm[0]);
+        //     }
+        // })
+        await dataArry.forEach((item) => {
+            let tempUseTime;
+            if(item.useTime === undefined) { tempUseTime = '정보 없음'; } 
+            else { tempUseTime = item.useTime[0]; }
+            station.create({
+                statNm : item.statNm[0],
+                statAddr : item.addrDoro[0],
+                chargerType : item.chgerType[0],
+                useTime : tempUseTime,
+                lat : item.lat[0],
+                lng : item.lng[0]
+            });
+        });
+    } catch (error) {
+        console.error(error);
+        return;
+    }
+});
+
+module.exports = {
+    insert
+}
