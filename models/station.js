4eb7daf1dad82ad35940706b605fc2ed35cdf905
MyeongSooChoi <mongus.c@gmail.com>
2020-04-23 18:27:11 +0900

    - 충전소 모델 구조 변경(공공 데이터에 맞는 자료형으로 변경 및 필요한 자료 추가)

--- models/station.js
+++ models/station.js
@@ -1,12 +1,36 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('station', {
-        stationName : {
-            type : DataTypes.STRING(20),
+        statNm : {
+            // 충전소 이름
+            type : DataTypes.STRING(100),
             allowNull : false
         },
-        stationAddress : {
-            type : DataTypes.STRING(100),
+        statAddr : {
+            // 충전소 주소
+            type : DataTypes.STRING(150),
             allowNull : false,
+        },
+        chargerType : {
+            /*
+                충전기 타입
+                01 : DC차데모
+                03 : DC차데모 + AC3상
+                06 : DC차데모 + AC3상 + DC콤보
+            */
+            type : DataTypes.STRING(4),
+            allowNull : false
+        },
+        useTime : {
+            type : DataTypes.STRING(50),
+            allowNull : false
+        },
+        lat : {
+            // 충전기 위도
+            type : DataTypes.FLOAT
+        },
+        lng : {
+            // 충전기 경도
+            type : DataTypes.FLOAT
         }
     }, {
         timestamps : false,
