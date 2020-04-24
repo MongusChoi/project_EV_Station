404936384ae52efebfe46304f683551b686e3c8d
MyeongSooChoi <mongus.c@gmail.com>
2020-04-24 15:29:57 +0900

    - 디버그 코드 삭제 - 현재 데이터 유무 판별 로직 변경

--- controllers/station.js
+++ controllers/station.js
@@ -7,7 +7,7 @@ const { station } = require('../models');
 // 0번 아이디 데이터를 조회해 데이터가 없으면 공공데이터 입력
 const insert = (async () => {
     try {
-        const exData = await station.findOne({ where : { id : '0' } });
+        const exData = await station.findOne({ where : { id : '1' } });
         if(exData) {
             // 현재 데이터가 들어가 있음
             console.log('already insert data');
@@ -22,17 +22,9 @@ const insert = (async () => {
                 return item.statNm[0];
             });
         });
-        let index = 0;
-        // await dataArry.forEach((item) => {
-        //     //console.log(index + ' ');
-        //     // console.log(index + ' ' + /*item.statNm[0] + ' , ' + */item.useTime/*[0]*/);
-        //     // ++index;
-        //     if(item.useTime === undefined) {
-        //         console.log(item.statNm[0]);
-        //     }
-        // })
         await dataArry.forEach((item) => {
-            let tempUseTime;
+            let tempUseTime;        
+            // 확인 결과 useTime만 undefined 정보가 들어 있어 판별해줌
             if(item.useTime === undefined) { tempUseTime = '정보 없음'; } 
             else { tempUseTime = item.useTime[0]; }
             station.create({
