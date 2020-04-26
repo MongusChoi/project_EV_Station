1615c31f8910a5871a9fbbcb6f1e79c8298eb4d4
MyeongSooChoi <mongus.c@gmail.com>
2020-04-26 16:38:43 +0900

    - map 렌더 시 appKey 전달하는 부분 추가

--- controllers/map.js
+++ controllers/map.js
@@ -4,6 +4,7 @@ const mapInit = (async(req, res, next) => {
     const stationData = await station.findAll();
     res.render('map', {
         user : req.user,
+        appKey : process.env.KAKAO_MAP,
         mapArr : stationData
     });
     //console.log(stationData[0].dataValues.statNm);
